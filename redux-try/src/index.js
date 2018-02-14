d7707a686fb31857a80beb93e6b01510cc070445
Feafortecsy <1228256157@qq.com>
2018-02-14 20:48:51 +0800

    共享结构的对象提高性能和reducer
    Author: Feafortecsy
    Date: 2018-02-14 20:48:30

--- redux-try/src/index.js
+++ redux-try/src/index.js
@@ -2,44 +2,91 @@
 // import ReactDOM from 'react-dom';
 // import './index.css';
 
-const appState = {
-    title: {
-        text: 'React.js 小书',
-        color: 'red',
-    },
-    content: {
-        text: 'React.js 小书内容',
-        color: 'blue'
-    }
-}
+// 与StoreChange合并
+// const appState = {
+//     title: {
+//         text: 'React.js 小书',
+//         color: 'red',
+//     },
+//     content: {
+//         text: 'React.js 小书内容',
+//         color: 'blue'
+//     }
+// }
 
 
-function createStore (state, storeChange) {
+function createStore (reducer) {
+    let state = null
     const listeners = []
+    // 监听数据变化
     const subscribe = (listener) => listeners.push(listener)
     const getState = () => state
     const dispatch = (action) => {
-        storeChange(state, action)
+        state = storeChange(state, action)
         listeners.forEach((listener) => listener())
     }
+    dispatch( { } )
     return { getState, dispatch, subscribe }
 }
 
 function storeChange (state, action) {
+    if (!state) {
+        return {
+            title: {
+                text: 'React.js 小书',
+                color: 'red',
+            },
+            content: {
+                text: 'React.js 小书内容',
+                color: 'blue'
+            }
+        } 
+    }
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text
-        break
+            return {
+                ...state,  
+                title: {
+                    ...state.title,
+                    text: action.text
+                }
+            }
         case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color
-        break
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    color: action.color
+                }
+            }
         default:
-        break
+            return state
     }
 }
 
 const store = createStore(appState, storeChange)
 let oldState = store.getState()
+store.subscribe(() => {
+    const newState = store.getState() // 数据可能变化，获取新的 state
+    renderApp(oldState, newState) // 把新旧的 state 传进去渲染
+    oldState = newState // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
+})
+
+// let newState = { // 新建一个 newAppState
+//     ...appState, // 复制 appState 里面的内容
+//     title: { // 用一个新的对象覆盖原来的 title 属性
+//         ...appState.title, // 复制原来 title 对象里面的内容
+//         text: '《React.js 小书》' // 覆盖 text 属性
+//     }
+// }
+
+// let newpState_1 = { // 新建一个 newAppState1
+//     ...newState, // 复制 newAppState1 里面的内容
+//     title: { // 用一个新的对象覆盖原来的 title 属性
+//         ...newState.title, // 复制原来 title 对象里面的内容
+//         color: "blue" // 覆盖 color 属性
+//     }
+// }
 
 store.subscribe(() => renderApp(store.getState()))
 
@@ -74,7 +121,6 @@ renderApp(store.getState())
 store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
 store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'lightblue' }) // 修改标题颜色
 // renderApp(store.getState()) // 把新的数据渲染到页面上
-
 
 // class App extends Component {
 //     render () {
--- seniorGuideNote.md
+++ seniorGuideNote.md
@@ -78,3 +78,30 @@ const a = 1
 const foo = (x, b) => x + b
 foo(1, 2) // => 3
 ```
+
+## REDUCER
+1. createStore 接受一个叫 reducer 的函数作为参数，这个函数规定是一个纯函数，它接受两个参数，一个是 state，一个是 action。
+
+2. 如果没有传入 state 或者 state 是 null，那么它就会返回一个初始化的数据。如果有传入 state 的话，就会根据 action 来“修改“数据，但其实它没有、也规定不能修改 state，而是要通过把修改路径的对象都复制一遍，然后产生一个新的对象返回。如果它不能识别你的 action，它就不会产生新的数据，而是（在 default 内部）把 state 原封不动地返回。
+
+3. reducer 是不允许有副作用的。你不能在里面操作 DOM，也不能发 Ajax 请求，更不能直接修改 state，它要做的仅仅是 —— 初始化和计算新的 state。
+
+4. 通用redux模式
+```
+// 定一个 reducer
+function reducer (state, action) {
+  /* 初始化 state 和 switch case */
+}
+
+// 生成 store
+const store = createStore(reducer)
+
+// 监听数据变化重新渲染页面
+store.subscribe(() => renderApp(store.getState()))
+
+// 首次渲染页面
+renderApp(store.getState()) 
+
+// 后面可以随意 dispatch 了，页面自动更新
+store.dispatch(...)
+```
